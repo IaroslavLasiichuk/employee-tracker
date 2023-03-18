bcc24863fdb03f4faeca55baa6578e3c21d352db
IaroslavLasiichuk <84236230+IaroslavLasiichuk@users.noreply.github.com>
2023-03-18 13:11:02 -0600

    Dotenv

---
+++ data.js
@@ -0,0 +1,44 @@
+const questions = [
+    // Departments
+    {
+      type: "input",
+      name: "department",
+      message: "View all departments",
+    },
+    // Roles
+    {
+      type: "input",
+      name: "role",
+      message: "View all roles",
+    },
+    // Employees
+    {
+      type: "input",
+      name: "employee",
+      message: "View all employees",
+    },
+    // Add a department
+    {
+      type: "input",
+      name: "add",
+      message: "Add a department",
+    },
+    // Add
+    {
+      type: "input",
+      name: "add",
+      message: "Add a roll",
+    },
+    {
+      type: "input",
+      name: "add",
+      message: "Add a employee",
+    },
+    {
+      type: "input",
+      name: "update",
+      message: "Update a employee role",
+    },
+]
+  
+module.exports = questions;
