bddb343ed73be9875afffe22f7e01d46392f6ee9
IaroslavLasiichuk <84236230+IaroslavLasiichuk@users.noreply.github.com>
2023-03-22 14:09:22 -0600

    Adds employee to database

--- db/query.sql
+++
@@ -1,9 +0,0 @@
--- SELECT * 
--- FROM department
--- INNER JOIN roles
--- ON department.id  = roles.department_id;
-
--- SELECT title, salary, department_id
--- FROM roles 
--- INNER JOIN employee
--- ON roles.id = employee.id;
